2f562a2ea2a699c3c3e3ffc142c38ff8d7ac8c5a
Ashish <kharbashish2121@gmail.com>
2023-07-11 19:27:12 +0530

    Show next player and winner messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import './styles.scss'
-import Board from './components/Board'
+import './styles.scss';
+import { useState } from 'react';
+import Board from './components/Board';
+import {calculateWinner} from './winner'
+
 function App() {
-  return <div className='app'>
-    <Board />
-  </div>
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [isXNext, setIsNext] = useState(false);
+
+    const winner = calculateWinner(squares);
+    const nextPlayer = isXNext ? 'X' : 'O';
+
+    const statusMessage = winner? `Winner is ${winner}` : `Next Player is ${nextPlayer}`;
+
+    const handleSquareClick = clickedPosition => {
+        if (squares[clickedPosition] || winner) {
+            return;
+        }
+
+        setSquares(currentSquares => {
+            return currentSquares.map((squareValue, position) => {
+                if (clickedPosition == position) {
+                    return isXNext ? 'X' : '0';
+                }
+                return squareValue;
+            });
+        });
+
+        setIsNext(currentIsXNext => {
+            return !currentIsXNext;
+        });
+    };
+
+    return (
+        <div className="app">
+            <h2>{statusMessage}</h2>
+            <Board squares={squares} handleSquareClick={handleSquareClick} />
+        </div>
+    );
 }
 
-export default App
+export default App;
