dc5ee73fa60da09ff3905ac9f747ec2f7b626329
Ashish <kharbashish2121@gmail.com>
2023-07-11 19:29:03 +0530

    applied prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './styles.scss';
 import { useState } from 'react';
 import Board from './components/Board';
-import {calculateWinner} from './winner'
+import { calculateWinner } from './winner';
 
 function App() {
     const [squares, setSquares] = useState(Array(9).fill(null));
@@ -10,7 +10,9 @@ function App() {
     const winner = calculateWinner(squares);
     const nextPlayer = isXNext ? 'X' : 'O';
 
-    const statusMessage = winner? `Winner is ${winner}` : `Next Player is ${nextPlayer}`;
+    const statusMessage = winner
+        ? `Winner is ${winner}`
+        : `Next Player is ${nextPlayer}`;
 
     const handleSquareClick = clickedPosition => {
         if (squares[clickedPosition] || winner) {
