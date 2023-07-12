7a64ba917c0767db480d6a6907c085a05d0c13b4
Ashish <kharbashish2121@gmail.com>
2023-07-12 11:09:30 +0530

    highlighted game winner

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -1,6 +1,5 @@
 const StatusMessage = ({ winner, gamingBoard }) => {
-
-    const {squares,isXNext} = gamingBoard;
+    const { squares, isXNext } = gamingBoard;
     const noMovesLeft = squares.every(squareValue => squareValue !== null);
 
     const nextPlayer = isXNext ? 'X' : 'O';
